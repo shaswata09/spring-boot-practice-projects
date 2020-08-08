89983655d30d325ddfb72d0dd6051bb2531796ec
shaswata09 <shaswatamitra@gmail.com>
2020-08-08 23:01:45 +0530

    [Video-3-08] Added path variable and printed in return string.

--- restful-web-services/src/main/java/com/leonardo/example/restfulwebservices/HelloWorldController.java
+++ restful-web-services/src/main/java/com/leonardo/example/restfulwebservices/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.leonardo.example.restfulwebservices;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,5 +15,10 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world-bean")
     public HelloWorldBean getHelloWorldBean() {
         return new HelloWorldBean("Hello World...");
+    }
+
+    @GetMapping(path = "/hello-world/path-variable/{name}")
+    public HelloWorldBean getHelloWorldPathParameter(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello %s ...", name));
     }
 }
