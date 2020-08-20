ba48d9f58676062db820cdb4c05741f93f22f2b9
shaswata09 <shaswatamitra@gmail.com>
2020-08-21 02:21:23 +0530

    [Video-3-22] Added internationalization using localeResolver

--- restful-web-services/src/main/java/com/leonardo/example/restfulwebservices/helloWorld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/leonardo/example/restfulwebservices/helloWorld/HelloWorldController.java
@@ -1,11 +1,19 @@
 package com.leonardo.example.restfulwebservices.helloWorld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private MessageSource messageSource;
 
     @GetMapping(path = "/hello-world")
     public String getHelloWorldString() {
@@ -20,5 +28,10 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world/path-variable/{name}")
     public HelloWorldBean getHelloWorldPathParameter(@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello %s ...", name));
+    }
+
+    @GetMapping(path = "/hello-world-internationalization")
+    public String getHelloWorldInternationalized(@RequestHeader(value = "Accept-Language", required = false) Locale locale) {
+        return messageSource.getMessage("good.morning.message", null, locale);
     }
 }
