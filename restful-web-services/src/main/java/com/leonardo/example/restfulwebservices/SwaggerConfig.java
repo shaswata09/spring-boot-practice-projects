7587201a7b374643592d492ec5be208aa2aba962
shaswata09 <shaswatamitra@gmail.com>
2020-08-22 19:34:58 +0530

    [Video-3-27] Added swagger and verification documentation

--- restful-web-services/src/main/java/com/leonardo/example/restfulwebservices/SwaggerConfig.java
+++ restful-web-services/src/main/java/com/leonardo/example/restfulwebservices/SwaggerConfig.java
@@ -6,20 +6,49 @@ import org.springframework.hateoas.client.LinkDiscoverer;
 import org.springframework.hateoas.client.LinkDiscoverers;
 import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
 import org.springframework.plugin.core.SimplePluginRegistry;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+
+    public static final Contact DEFAULT_CONTACT = new Contact(
+            "Leonrdo",
+            "dummyurl.com",
+            "shaswatamitra@gmail.com");
+
+    public static final ApiInfo DEFAULT =
+            new ApiInfo(
+                    "restful-web-services swagger",
+                    "restful-web-services Documentation",
+                    "1.0",
+                    "urn:tos",
+                    DEFAULT_CONTACT,
+                    "Apache 2.0",
+                    "http://www.apache.org/licenses/LICENSE-2.0", new ArrayList());
+
+    public static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES = new HashSet<String>(
+            Arrays.asList("application/json", "application/xml"));
 
     @Bean
     public LinkDiscoverers discoverers() {
         List<LinkDiscoverer> plugins = new ArrayList<>();
         plugins.add(new CollectionJsonLinkDiscoverer());
         return new LinkDiscoverers(SimplePluginRegistry.create(plugins));
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(DEFAULT)
+                .produces(DEFAULT_PRODUCES_AND_CONSUMES)
+                .consumes(DEFAULT_PRODUCES_AND_CONSUMES);
     }
 
 }
--- restful-web-services/src/main/java/com/leonardo/example/restfulwebservices/user/User.java
+++ restful-web-services/src/main/java/com/leonardo/example/restfulwebservices/user/User.java
@@ -1,17 +1,23 @@
 package com.leonardo.example.restfulwebservices.user;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
+@ApiModel(description = "User Model description")
 public class User {
     private Integer id;
 
+    @ApiModelProperty(notes = "Name sould be at least 2 chars")
     @NotNull(message = "Name should not be null")
     @Size(min=2, message = "Name should have at least 2 characters")
     private String name;
 
+    @ApiModelProperty(notes = "DOB should be in past")
     @NotNull(message = "DOB should not be null")
     @Past(message = "DOB should be in past")
     private Date date;
