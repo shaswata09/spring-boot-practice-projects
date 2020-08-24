f6290aad452101e57db1b961cd09df365ef3b165
shaswata09 <shaswatamitra@gmail.com>
2020-08-25 03:26:29 +0530

    [Video-3-32] Added URIs versioning using content negotiation and headers

--- restful-web-services/src/main/java/com/leonardo/example/restfulwebservices/PersonVersionController.java
+++ restful-web-services/src/main/java/com/leonardo/example/restfulwebservices/PersonVersionController.java
@@ -20,4 +20,35 @@ public class PersonVersionController {
         return new PersonV2(new Name("Bob", "Charlie"));
     }
 
+    @GetMapping(path = "/person/param", params = "version=1")
+    public PersonV1 paramsPersonV1 (){
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping(value = "/person/param", params = "version=2")
+    public PersonV2 paramsPersonV2 (){
+        return new PersonV2(new Name("Bob", "Charlie"));
+    }
+
+    @GetMapping(path = "/person/header", headers = "X-API-VERSION=1")
+    public PersonV1 headerPersonV1 (){
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping(value = "/person/header", headers = "X-API-VERSION=2")
+    public PersonV2 headerPersonV2 (){
+        return new PersonV2(new Name("Bob", "Charlie"));
+    }
+
+    @GetMapping(path = "/person/produces", produces = "application/vnd.company.app-v1+json")
+    public PersonV1 producesPersonV1 (){
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping(value = "/person/produces", produces = "application/vnd.company.app-v2+json")
+    public PersonV2 producesPersonV2 (){
+        return new PersonV2(new Name("Bob", "Charlie"));
+    }
+
+
 }
