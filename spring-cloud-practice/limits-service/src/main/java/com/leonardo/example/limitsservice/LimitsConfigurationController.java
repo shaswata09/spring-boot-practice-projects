ce62f05ade69c96705fe52efbbe5fa2593a47881
Shaswata Mitra <shaswatamitra@gmail.com>
2020-09-01 03:49:37 +0530

    [Video-4-10] Enhanced limit service to fetch configuration values from application.properties

--- spring-cloud-practice/limits-service/src/main/java/com/leonardo/example/limitsservice/LimitsConfigurationController.java
+++ spring-cloud-practice/limits-service/src/main/java/com/leonardo/example/limitsservice/LimitsConfigurationController.java
@@ -1,14 +1,18 @@
 package com.leonardo.example.limitsservice;
 
 import com.leonardo.example.limitsservice.bean.LimitConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsConfigurationController {
 
-    @GetMapping (path = "/limits")
-    public LimitConfiguration retrieveLimitsFromConfigurations (){
-        return new LimitConfiguration(1, 1000);
+    @Autowired
+    private Configuration configuration;
+
+    @GetMapping(path = "/limits")
+    public LimitConfiguration retrieveLimitsFromConfigurations() {
+        return new LimitConfiguration(configuration.getMinimum(), configuration.getMaximum());
     }
 }
