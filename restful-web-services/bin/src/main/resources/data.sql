bf15f7d26d882ef5c6827cf4d7d74440bb5842a6
shaswata09 <shaswatamitra@gmail.com>
2020-12-08 01:54:45 +0530

    Added index.html as server home page.

---
+++ restful-web-services/bin/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into user values (1001, sysdate(), 'Abc');
+insert into user values (1002, sysdate(), 'Def');
+insert into user values (1003, sysdate(), 'Ghi');
+insert into user values (1004, sysdate(), 'Jkl');
+insert into user values (1005, sysdate(), 'Mno');
+
+insert into post values (1101, 'My first post', 1001);
+insert into post values (1102, 'My second post', 1001);
+insert into post values (1103, 'My third post', 1002);
