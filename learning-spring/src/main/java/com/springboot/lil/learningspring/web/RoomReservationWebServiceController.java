2da356406171152d9826892f87241778d4cea910
Shaswata Mitra <shaswatamitra@gmail.com>
2020-12-20 20:04:15 +0530

    [Video-5/2] Added getReservationByDate Service api

---
+++ learning-spring/src/main/java/com/springboot/lil/learningspring/web/RoomReservationWebServiceController.java
@@ -0,0 +1,32 @@
+package com.springboot.lil.learningspring.web;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springboot.lil.learningspring.business.domain.RoomReservation;
+import com.springboot.lil.learningspring.business.service.ReservationService;
+
+@RestController
+@RequestMapping("/api/reservations")
+public class RoomReservationWebServiceController {
+
+	private final ReservationService reservationService;
+
+	@Autowired
+	public RoomReservationWebServiceController(ReservationService reservationService) {
+		this.reservationService = reservationService;
+	}
+
+	@GetMapping
+	public List<RoomReservation> getRoomReservations(@RequestParam(name = "date", required = false) String dateString) {
+		Date date = DateUtils.createDateFromDateString(dateString);
+		return this.reservationService.getRoomReservationforDate(date);
+	}
+
+}
